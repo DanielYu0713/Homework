a4c9730c49958da3d33e9083bc6a01e61b1e80f4
DanielYu0713 <a46720438@gmail.com>
2022-10-11 20:44:20 +0800

    Create s1091438_Container.h

---
+++ 程式設計(二)/1091438-HW1/s1091438_Container.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<vector>
+#include"s1091438_Date.h"
+using namespace std;
+class Container
+{
+public:
+	Container(int); //建立空容器（輸入值為容器大小）
+	void addDate(Date);
+	void removeLast(); //移除最後放入的（沒得移時無作用）
+	void printALL(); //印出所有日期
+private:
+	vector <Date> dates2;
+};
